cb296e02af13691d87df4337d437d9c8a23a4d3f
unknown <nikitatk15@gmail.com>
2019-02-25 16:57:10 +0530

    tp

---
+++ application/views/base_web/register.php
@@ -0,0 +1,109 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+    <title>Material Pro Admin Template - The Most Complete &amp; Trusted Bootstrap 4 Admin Template</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- You can change the theme colors from here -->
+    <link href="css/colors/default-dark.css" id="theme" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<style type="text/css">.jqstooltip { position: absolute;left: 0px;top: 0px;visibility: hidden;background: rgb(0, 0, 0) transparent;background-color: rgba(0,0,0,0.6);filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000);-ms-filter: "progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)";color: white;font: 10px arial, san serif;text-align: left;white-space: nowrap;padding: 5px;border: 1px solid white;z-index: 10000;}.jqsfield { color: white;font: 10px arial, san serif;text-align: left;}</style></head>
+
+
+<body>
+   <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Form Validation</h4>
+                                <h6 class="card-subtitle">Bootstrap Form Validation check the <a href="http://reactiveraven.github.io/jqBootstrapValidation/">official website </a></h6>
+                                <form class="m-t-40 error" novalidate="">
+                                    <div class="form-group validate">
+                                        <h5>Basic Text Input <span class="text-danger">*</span></h5>
+                                        <div class="controls">
+                                            <input type="text" name="text" class="form-control" required="" data-validation-required-message="This field is required" aria-invalid="false"> <div class="help-block"></div></div>
+                                        <div class="form-control-feedback"><small>Add <code>required</code> attribute to field for required validation.</small></div>
+                                    </div>
+                                    <div class="form-group issue">
+                                        <h5>Email Field <span class="text-danger">*</span></h5>
+                                        <div class="controls">
+                                            <input type="email" name="email" class="form-control" required="" data-validation-required-message="This field is required" aria-invalid="true"> <div class="help-block"><ul role="alert"><li>Not a valid email address<!-- data-validation-email-message to override --></li></ul></div></div>
+                                    </div>
+                                    <div class="form-group validate">
+                                        <h5>Password Input Field <span class="text-danger">*</span></h5>
+                                        <div class="controls">
+                                            <input type="password" name="password" class="form-control" required="" data-validation-required-message="This field is required" aria-invalid="false"> <div class="help-block"></div></div>
+                                    </div>
+                                    <div class="form-group error">
+                                        <h5>Repeat Password Input Field <span class="text-danger">*</span></h5>
+                                        <div class="controls">
+                                            <input type="password" name="password2" data-validation-match-match="password" class="form-control" required="" aria-invalid="true"> <div class="help-block"><ul role="alert"><li>Must match</li></ul></div></div>
+                                    </div>
+                                    <div class="form-group error">
+                                        <h5>File Input Field <span class="text-danger">*</span></h5>
+                                        <div class="controls">
+                                            <input type="file" name="file" class="form-control" required="" aria-invalid="true"> <div class="help-block"><ul role="alert"><li>This is required<!-- data-validator-required-message to override --></li></ul></div></div>
+                                    </div>
+                                    <div class="form-group error">
+                                        <h5>Input with Icon <span class="text-danger">*</span></h5>
+                                        <div class="controls">
+                                            <div class="input-group">
+                                                <input type="text" class="form-control" placeholder="Addon To Right" data-validation-required-message="This field is required" aria-invalid="true">
+                                                <div class="input-group-append">
+                                                    <span class="input-group-text">$</span>
+                                                </div>
+                                            </div>
+
+ <div class="text-xs-right">
+                                                <button type="submit" class="btn btn-info">Submit</button>
+                                                <button type="reset" class="btn btn-inverse">Cancel</button>
+                                            </div>
+                                             
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+ <script src="../assets/plugins/jquery/jquery.min.js"></script>
+        <!-- Bootstrap tether Core JavaScript -->
+        <script src="../assets/plugins/popper/popper.min.js"></script>
+        <script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+        <!-- slimscrollbar scrollbar JavaScript -->
+        <script src="js/jquery.slimscroll.js"></script>
+        <!--Wave Effects -->
+        <script src="js/waves.js"></script>
+        <!--Menu sidebar -->
+        <script src="js/sidebarmenu.js"></script>
+        <!--stickey kit -->
+        <script src="../assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+        <script src="../assets/plugins/sparkline/jquery.sparkline.min.js"></script>
+        <!--Custom JavaScript -->
+        <script src="js/custom.min.js"></script>
+        <script src="js/validation.js"></script>
+        <script>
+        ! function(window, document, $) {
+            "use strict";
+            $("input,select,textarea").not("[type=submit]").jqBootstrapValidation()
+        }(window, document, jQuery);
+        </script>
+        <!-- ============================================================== -->
+        <!-- Style switcher -->
+        <!-- ============================================================== -->
+        <script src="../assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
+
+                </div>
+</body>
