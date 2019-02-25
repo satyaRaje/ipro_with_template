4a063981bf7a7928584eff11782bc02c2317cfd3
data <joshipoonam207@gmail.com>
2019-02-25 05:01:30 -0500

    contact and login

--- application/views/base_web/contact_us.php
+++ application/views/base_web/contact_us.php
@@ -1,53 +1,34 @@
+<div class="row" style="padding-left: 20%;padding-right: 20%">
+                      <div class="col-lg-12">
+                      <div class="card">
+                        <div class="card-body" >
+                            <h4 class="card-title">Contact Us</h4>
 
-<body>
-    <!-- ============================================================== -->
-    <!-- Preloader - style you can find in spinners.css -->
-    <!-- ============================================================== -->
-    <div class="preloader">
-        <svg class="circular" viewBox="25 25 50 50">
-            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
-    </div>
-    <!-- ============================================================== -->
-    <!-- Main wrapper - style you can find in pages.scss -->
-    <!-- ============================================================== -->
-    <div id="main-wrapper">
-        <!-- ============================================================== -->
-        <!-- Topbar header - style you can find in pages.scss -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- End Topbar header -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Left Sidebar - style you can find in sidebar.scss  -->
-        <!-- ============================================================== -->
-            <!-- End Sidebar scroll-->
-            <!-- Bottom points-->
-            <!-- End Bottom points-->
-        </aside></div>
-                <!-- ============================================================== -->
-                
-                <!-- Row -->
-                <!-- Row -->
-                <div class="row">
-                    <div class="col-lg-12" style="padding-left: 20%;padding-right: 20%">
-                        <div class="card">
-                            <div class="card-body">
-                                <h4 class="card-title">Sample Form with the Icons</h4>
-                                <h6 class="card-subtitle">made with bootstrap elements</h6>
-                                <form class="form p-t-20">
+                     <form class="form p-t-20">
                                     <div class="form-group">
-                                        <label for="exampleInputuname">User Name</label>
+                                        <label for="exampleInputuname">Name</label>
                                         <div class="input-group">
                                             <div class="input-group-prepend">
                                                 <span class="input-group-text" id="basic-addon1">
                                                     <i class="ti-user"></i>
                                                 </span>
                                             </div>
-                                            <input type="text" class="form-control" id="exampleInputuname" placeholder="Username">
+                                            <input type="text" class="form-control" id="exampleInputuname" placeholder="Name">
+                                        </div>
+                                    </div>
+                                     <div class="form-group">
+                                        <label for="exampleInputEmail1">Mobile Number</label>
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text" id="basic-addon1">
+                                                    <i class="ti-mobile"></i>
+                                                </span>
+                                            </div>
+                                            <input type="email" class="form-control" id="exampleInputMobile" placeholder="Enter Mobile Number">
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label for="exampleInputEmail1">Email address</label>
+                                        <label for="exampleInputEmail1">Email</label>
                                         <div class="input-group">
                                             <div class="input-group-prepend">
                                                 <span class="input-group-text" id="basic-addon1">
@@ -57,260 +38,48 @@
                                             <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
                                         </div>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="pwd1">Password</label>
-                                        <div class="input-group">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text" id="basic-addon1">
-                                                    <i class="ti-lock"></i>
-                                                </span>
-                                            </div>
-                                            <input type="password" class="form-control" id="pwd1" placeholder="Enter email">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="pwd2">Confirm Password</label>
-                                        <div class="input-group">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text" id="basic-addon1">
-                                                    <i class="ti-lock"></i>
-                                                </span>
-                                            </div>
-                                            <input type="password" class="form-control" id="pwd2" placeholder="Enter email">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="checkbox checkbox-success">
-                                            <input id="checkbox1" type="checkbox">
-                                            <label for="checkbox1"> Remember me </label>
-                                        </div>
-                                    </div>
-                                    <button type="submit" class="btn btn-success waves-effect waves-light m-r-10">Submit</button>
+                                   <div class="form-group">
+                                    <label>Info</label>
+                                    <select class="custom-select col-12" id="inlineFormCustomSelect">
+                                        <option selected="">select</option>
+                                        <option value="worker">Student</option>
+                                        <option value="businessman">Industry</option>
+                                        <option value="student">Employee</option>
+                                        <option value="student">Designer</option>
+                                        <option value="student">Maker</option>
+                                        <option value="student">Professional</option>
+                                        <option value="other">Other</option>
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <label>How you know about us</label>
+                                    <select class="custom-select col-12" id="inlineFormCustomSelect">
+                                        <option selected="">select</option>
+                                        <option value="news">Newspaper</option>
+                                        <option value="news">Google</option>
+                                        <option value="socialsites">Social Media</option>
+                                        <option value="college">College</option>
+                                        <option value="workplace">Working Place</option>
+                                        <option value="friend">Friends</option>
+                                        <option value="other">Other</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label>Message</label>
+                                  
+                                    <textarea class="form-control" rows="5">
+                                </textarea> </div>
+                                  
+                                     <button type="submit" class="btn btn-success waves-effect waves-light m-r-10">Send Message</button>
                                     <button type="submit" class="btn btn-inverse waves-effect waves-light">Cancel</button>
                                 </form>
                             </div>
                         </div>
                     </div>
-                       <!-- All Jquery -->
-    <!-- ============================================================== -->
-    <script src="../assets/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap tether Core JavaScript -->
-    <script src="../assets/plugins/popper/popper.min.js"></script>
-    <script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-    <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="js/jquery.slimscroll.js"></script>
-    <!--Wave Effects -->
-    <script src="js/waves.js"></script>
-    <!--Menu sidebar -->
-    <script src="js/sidebarmenu.js"></script>
-    <!--stickey kit -->
-    <script src="../assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
-    <script src="../assets/plugins/sparkline/jquery.sparkline.min.js"></script>
-    <!--Custom JavaScript -->
-    <script src="js/custom.min.js"></script>
-    <!-- ============================================================== -->
-    <!-- Style switcher -->
-    <!-- ============================================================== -->
-    <script src="../assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
+                </div>
+                
+                 </body>
 
+                    
 
-</body></html>        <!-- ============================================================== -->
-                <!-- End PAge Content -->
-                <!-- ============================================================== -->
-            </div>
-            <!-- ============================================================== -->
-            <!-- End Container fluid  -->
-            <!-- ============================================================== -->
-            <!-- ============================================================== -->
-            <!-- footer -->
-            <!-- ============================================================== -->
-            
-            <!-- ============================================================== -->
-            <!-- End footer -->
-            <!-- ============================================================== -->
-        </div>
-        <!-- ============================================================== -->
-        <!-- End Page wrapper  -->
-        <!-- ============================================================== -->
-    </div>
-    <!-- ============================================================== -->
-    <!-- End Wrapper -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- All Jquery -->
-    <!-- ============================================================== -->
-    <script src="<?php echo base_url();?>assets/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap tether Core JavaScript -->
-    <script src="<?php echo base_url();?>assets/plugins/popper/popper.min.js"></script>
-    <script src="<?php echo base_url();?>assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-    <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="<?php echo base_url();?>material/js/jquery.slimscroll.js"></script>
-    <!--Wave Effects -->
-    <script src="<?php echo base_url();?>material/js/waves.js"></script>
-    <!--Menu sidebar -->
-    <script src="<?php echo base_url();?>material/js/sidebarmenu.js"></script>
-    <!--stickey kit -->
-    <script src="<?php echo base_url();?>assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
-    <script src="<?php echo base_url();?>assets/plugins/sparkline/jquery.sparkline.min.js"></script>
-    <!--Custom JavaScript -->
-    <script src="<?php echo base_url();?>material/js/custom.min.js"></script>
-    <script src="<?php echo base_url();?>assets/plugins/prism/prism.js"></script>
-    <script>
-    // This is for the sticky sidebar    
-    $("#sidebarnav").stick_in_parent({
-        offset_top: 100
-    });
-    $('#sidebarnav a').click(function() {
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - 100
-        }, 500);
-        return false;
-    });
-    // This is auto select left sidebar
-    // Cache selectors
-    // Cache selectors
-    var lastId,
-        topMenu = $("#sidebarnav"),
-        topMenuHeight = topMenu.outerHeight(),
-        // All list items
-        menuItems = topMenu.find("a"),
-        // Anchors corresponding to menu items
-        scrollItems = menuItems.map(function() {
-            var item = $($(this).attr("href"));
-            if (item.length) {
-                return item;
-            }
-        });
-
-    // Bind click handler to menu items
-
-
-    // Bind to scroll
-    $(window).scroll(function() {
-        // Get container scroll position
-        var fromTop = $(this).scrollTop() + topMenuHeight - 250;
-
-        // Get id of current scroll item
-        var cur = scrollItems.map(function() {
-            if ($(this).offset().top < fromTop)
-                return this;
-        });
-        // Get the id of the current element
-        cur = cur[cur.length - 1];
-        var id = cur && cur.length ? cur[0].id : "";
-
-        if (lastId !== id) {
-            lastId = id;
-            // Set/remove active class
-            menuItems
-                .removeClass("active")
-                .filter("[href='#" + id + "']").addClass("active");
-        }
-    });
-    </script>
-    <!-- ============================================================== -->
-    <!-- Style switcher -->
-    <!-- ============================================================== -->
-    <script src="<?php echo base_url();?>assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
-</body>
-
-</html>
-      <!-- ============================================================== -->
-            <!-- End Container fluid  -->
-            <!-- ============================================================== -->
-            <!-- ============================================================== -->
-            <!-- footer -->
-            <!-- ============================================================== -->
-            <footer class="footer">
-                © 2019 Material Pro Admin by wrappixel.com
-            </footer>
-            <!-- ============================================================== -->
-            <!-- End footer -->
-            <!-- ============================================================== -->
-        </div>
-        <!-- ============================================================== -->
-        <!-- End Page wrapper  -->
-        <!-- ============================================================== -->
-    </div>
-    <!-- ============================================================== -->
-    <!-- End Wrapper -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- All Jquery -->
-    <!-- ============================================================== -->
-    <script src="<?php echo base_url();?>assets/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap tether Core JavaScript -->
-    <script src="<?php echo base_url();?>assets/plugins/popper/popper.min.js"></script>
-    <script src="<?php echo base_url();?>assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-    <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="<?php echo base_url();?>material/js/jquery.slimscroll.js"></script>
-    <!--Wave Effects -->
-    <script src="<?php echo base_url();?>material/js/waves.js"></script>
-    <!--Menu sidebar -->
-    <script src="<?php echo base_url();?>material/js/sidebarmenu.js"></script>
-    <!--stickey kit -->
-    <script src="<?php echo base_url();?>assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
-    <script src="<?php echo base_url();?>assets/plugins/sparkline/jquery.sparkline.min.js"></script>
-    <!--Custom JavaScript -->
-    <script src="<?php echo base_url();?>material/js/custom.min.js"></script>
-    <script src="<?php echo base_url();?>assets/plugins/prism/prism.js"></script>
-    <script>
-    // This is for the sticky sidebar    
-    $("#sidebarnav").stick_in_parent({
-        offset_top: 100
-    });
-    $('#sidebarnav a').click(function() {
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - 100
-        }, 500);
-        return false;
-    });
-    // This is auto select left sidebar
-    // Cache selectors
-    // Cache selectors
-    var lastId,
-        topMenu = $("#sidebarnav"),
-        topMenuHeight = topMenu.outerHeight(),
-        // All list items
-        menuItems = topMenu.find("a"),
-        // Anchors corresponding to menu items
-        scrollItems = menuItems.map(function() {
-            var item = $($(this).attr("href"));
-            if (item.length) {
-                return item;
-            }
-        });
-
-    // Bind click handler to menu items
-
-
-    // Bind to scroll
-    $(window).scroll(function() {
-        // Get container scroll position
-        var fromTop = $(this).scrollTop() + topMenuHeight - 250;
-
-        // Get id of current scroll item
-        var cur = scrollItems.map(function() {
-            if ($(this).offset().top < fromTop)
-                return this;
-        });
-        // Get the id of the current element
-        cur = cur[cur.length - 1];
-        var id = cur && cur.length ? cur[0].id : "";
-
-        if (lastId !== id) {
-            lastId = id;
-            // Set/remove active class
-            menuItems
-                .removeClass("active")
-                .filter("[href='#" + id + "']").addClass("active");
-        }
-    });
-    </script>
-    <!-- ============================================================== -->
-    <!-- Style switcher -->
-    <!-- ============================================================== -->
-    <script src="<?php echo base_url();?>assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
-</body>
-
-</html>
---
+++ application/views/base_web/login.php
@@ -0,0 +1,46 @@
+<div class="row" style="padding-left: 20%;padding-right: 20%">
+                      <div class="col-lg-12">
+                      <div class="card">
+                        <div class="card-body" >
+                            <h4 class="card-title">Login</h4>
+<form class="form p-t-20">
+                                    <div class="form-group">
+                                        <label for="exampleInputuname">User Name /Email /Mobile Number</label>
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text" id="basic-addon1">
+                                                    <i class="ti-user"></i>
+                                                </span>
+                                            </div>
+                                            <input type="text" class="form-control" id="exampleInputuname" placeholder="Username">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="pwd1">Password</label>
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text" id="basic-addon1">
+                                                    <i class="ti-lock"></i>
+                                                </span>
+                                            </div>
+                                            <input type="password" class="form-control" id="pwd1" placeholder="Enter Password">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="checkbox checkbox-success">
+                                            <input id="checkbox1" type="checkbox">
+                                            <label for="checkbox1"> Remember me </label>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-success waves-effect waves-light m-r-10">Submit</button>
+                                    <button type="submit" class="btn btn-inverse waves-effect waves-light">Cancel</button>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </body>
+
+                    
+
