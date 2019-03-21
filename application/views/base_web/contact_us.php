068f215a17e815ede0d158dc7bdbd68aacaed149
NikitaKolte <nikitatk15@gmail.com>
2019-03-22 00:15:38 +0530

    done

--- application/views/base_web/contact_us.php
+++ application/views/base_web/contact_us.php
@@ -1,33 +1,152 @@
 <title>IPRO3D-Contact_Us</title>
-<body>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+    
+    <!-- Bootstrap Core CSS -->
+    <link href="http://ipro3d.io/final/ipro_with_template/style/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- owl slider CSS -->
+    <link href="http://ipro3d.io/final/ipro_with_template/style/assets/owl.carousel/owl.carousel.min.css" rel="stylesheet">
+    <link href="http://ipro3d.io/final/ipro_with_template/style/assets/owl.carousel/owl.theme.default.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="http://ipro3d.io/final/ipro_with_template/style/css/style.css" rel="stylesheet">
+    <!-- HTML5 Shim and R   espond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script async="" src="https://www.google-analytics.com/analytics.js"></script><script async="" src="https://www.google-analytics.com/analytics.js"></script><script>
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-85622565-1', 'auto');
+    ga('send', 'pageview');
+    </script>
+</head>
+
+<body class="fix-header" style="background-image: url(http://ipro3d.io/final/ipro_with_template/images/slider/contact_us.jpg);">
+<!-- ============================================================== -->
+<!-- Preloader - style you can find in spinners.css -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- Main wrapper - style you can find in pages.scss -->
+<!-- ============================================================== -->
+<div id="main-wrapper">
+    <!-- ============================================================== -->
+    <!-- Topbar header - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    </div>
+
+    <script src="http://ipro3d.io/final/ipro_with_template/js/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="http://ipro3d.io/final/ipro_with_template/bootstrap/js/popper.min.js"></script>
+    <script src="http://ipro3d.io/final/ipro_with_template/js/waves.js"></script>
+    <script src="http://ipro3d.io/final/ipro_with_template/js/sticky-kit.min.js"></script>
+    <script src="http://ipro3d.io/final/ipro_with_template/assets/owl.carousel/owl.carousel.min.js"></script>
+    <script src="http://ipro3d.io/final/ipro_with_template/js/custom.min.js"></script>
+
+
+
+
+    <!-- ============================================================== -->
+    <!-- Page wrapper  -->
+    <!-- ============================================================== -->
+    <div class="page-wrapper">
+        <!-- ============================================================== -->
+        <!-- Container fluid  -->
+        <!-- ============================================================== -->
+        <div class="container-fluid">
+
+
+            <!-- ============================================================== -->
+            <!-- Testimonial -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Call to action bar -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+<title>IPRO3D-Contact_Us</title>
+
     <div class="preloader" style="display: none;">
         <svg class="circular" viewBox="25 25 50 50">
             <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"></circle> </svg>
+    
     </div>
-    <div class="ribbon-wrapper card">
-                                    <div class="ribbon ribbon-default">Ribbon</div>
-                                    <p class="ribbon-content">Duis mollis, est non commodo luctus, nisi erat porttitor ligula</p>
-                                </div>
-<div class="row" style="padding-left: 40%;padding-right: 5%;">
-                      <div class="col-lg-8"><br>
-                      <div class="card">
-                        <div class="card-body" style="background-color: #F5F5F5F5;background-image:url('<?php echo base_url();?>assets/images/slider/b1.png')">
+    <div style="background-image: url(http://ipro3d.io/final/ipro_with_template/images/slider/contact_us.jpg);background-position: center; ">
+    <br><br>
+    
+<div class="row" style="padding-left: 20%;padding-right: 20%;">
+                      <div class="col-lg-12">
+                          <div class="card" style="">
+                              <div class="row" style="">
+                              <div class="col-lg-6">
+                                  <h2 class="text text-primary"><b>Address</b></h2>
+                                <h4 class="text font-normal m-l-10">
+                               <b> IPRO3D TECHNOLOGIES LLP
+                                  Incubation Office </b><br>
+                                C/O Er. Yogesh Pawar,<br>
+                                Workstation-1, MAGIC, CMIA office,<br>
+                                Bajaj Bhavan, Near MIDC office,<br>
+                                Railway Station Road, Aurangabad,<br>
+                                Maharashtra, India.
+                                </h4>
+                             <br>
+                             <h4 class="text font-normal m-l-10">
+                               <b> Corporate office Manufacturing Facility</b><br>
+                            C/O Er. Yogesh Pawar,<br>
+                            Meridian Status-B, F-7,<br>
+                            Near Youth Hostel, Kesarsingpura,<br>
+                            Aurangabad-431005, Maharashtra,<br>
+                            India.
+                            </h4>
+
+                             <br>
+                             <h4 class="text font-normal m-l-10">
+                               <b> Address 3</b><br>
+                            C/O Er. Yogesh Pawar,<br>
+                            14, Erandale Avenue<br>
+                            Ripponlea, Melbourn,<br>
+                            VIC 3183, Australia.<br>
+                            </h4>
+                            
+                            <br><br>
+                            <h3 class="text font-normal m-l-10"><b>Email: hello@ipro3d.io<br>
+                                Mobile / Whatsapp: 9821413663 </b></h3>
+                            
+                              </div>
+                          <div class=" col-lg-6">
+                        <div class="card-body">
                             <h2 class="card-title" id="printing" align="center" style="font-weight: bold;font-family: Philosiphers">Contact Us</h2>
 
-                     <form class="form p-t-20" method="post" action="<?php echo base_url()."index.php/dynamic/enquiry/enquiry/add_contact"?>">
+                     <form class="form p-t-20" method="post" action="http://ipro3d.io/final/ipro_with_template/index.php/dynamic/enquiry/enquiry/add_contact">
                                     <div class="form-group">
-                                        <label for="exampleInputuname"><b>Name</b></label>
+                                        <label for="exampleInputuname">Name</label>
                                         <div class="input-group">
                                             <div class="input-group-prepend">
                                                 <span class="input-group-text" id="basic-addon1">
                                                     <i class="ti-user"></i>
                                                 </span>
                                             </div>
-                                            <input type="text" class="form-control" name="fname" placeholder="Enter Name">
+                                            <input type="text" class="form-control" name="fname" placeholder="Name">
                                         </div>
                                     </div>
                                      <div class="form-group">
-                                        <label><b>Mobile Number</b></label>
+                                        <label>Mobile Number</label>
                                         <div class="input-group">
                                             <div class="input-group-prepend">
                                                 <span class="input-group-text" id="basic-addon1">
@@ -38,7 +157,7 @@
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label for="exampleInputEmail1"><b>Email</b></label>
+                                        <label for="exampleInputEmail1">Email</label>
                                         <div class="input-group">
                                             <div class="input-group-prepend">
                                                 <span class="input-group-text" id="basic-addon1">
@@ -49,7 +168,7 @@
                                         </div>
                                     </div>
                                    <div class="form-group">
-                                    <label><b>Info</b></label>
+                                    <label>Info</label>
                                     <select class="custom-select col-12" name="inlineFormCustomSelect">
                                         <option selected="">select</option>
                                         <option value="worker">Student</option>
@@ -63,7 +182,7 @@
                                 </div>
 
                                 <div class="form-group">
-                                    <label><b>How you know about us</b></label>
+                                    <label>How you know about us</label>
                                     <select class="custom-select col-12" name="inlineFormCustomSelect2">
                                         <option selected="">select</option>
                                         <option value="news">Newspaper</option>
@@ -76,50 +195,59 @@
                                     </select>
                                 </div>
                                 <div class="form-group">
-                                    <label><b>Message</b></label>
+                                    <label>Message</label>
                                   
-                                    <textarea class="form-control" rows="5" name="message">
-                                </textarea> </div>
+                                    <textarea class="form-control" rows="5" name="message">                                </textarea> </div>
                                   
                                      <button type="submit" class="btn btn-success waves-effect waves-light m-r-10" name="send">Send Message</button>
                                     <button type="submit" class="btn btn-inverse waves-effect waves-light">Cancel</button>
                                 </form>
                             </div>
+                          </div>
                         </div>
+                          </div>
                     </div>
+
                 </div>
-                <br>
-                <div class="row" style="background-color: #bbbbbb;">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3752.2523532716305!2d75.31352311425753!3d19.871563331647998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bdb99e84cc7f4e7%3A0x8ef52048513cb04!2sIPRO3D+Technologies+LLP!5e0!3m2!1sen!2sin!4v1534930979184" style="width:100%;height: 400px" frameborder="0" style="border:0" allowfullscreen></iframe>
-    </div>
-<br><br><br>
-<hr>
-    <div class="container">
-        <div class="row" align="center">
-            <div class="col-md-3" >
-            </div>
-            <div class="col-md-6" >
-                <div class="thumbnail center well well-sm text-center">
-                    <h2>Newsletter</h2> 
-                    <p>Subscribe to our Newsletter and stay tuned.</p>
-                    <form action="" method="post" role="form">
-                        <div class="input-group">
-                          <span class="input-group-text">
-                            <i class="fa fa-envelope"></i>
-                          </span>
-                          <input class="form-control" type="text" id="" name="" placeholder="your@email.com">
-                        </div><br>
-                        <input type="submit" value="Subscribe" class="btn btn-large btn-primary" />
-                  </form>
-                </div>    
-            </div>
-        </div><br>
-    </div>
-
-
-                 </body>
+                </div>
+                 
 
                     
+<!---->
+<!-- End Page Content -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- footer -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- End footer -->
+<!-- ============================================================== -->
+</div>
+<!-- ============================================================== -->
+<!-- End Container fluid  -->
+<!-- ============================================================== -->
+</div>
+<!-- ============================================================== -->
+<!-- End Page wrapper  -->
+<!-- ============================================================== -->
+
+<!-- ============================================================== -->
+<!-- End Wrapper -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- All Jquery -->
+<!-- ============================================================== -->
+
+<!-- jQuery for carousel -->
+<script src="http://ipro3d.io/final/ipro_with_template/style/assets/owl.carousel/owl.carousel.min.js"></script>
+<!--Custom JavaScript -->
+<script src="http://ipro3d.io/final/ipro_with_template/style/js/custom.min.js"></script>
+
+
+
+</body>
+</html>
+                   
 <?php
 if(isset($_POST['send'])){
   /*  $query= "INSERT INTO `tblEnquiry`(`name`, `mobile`, `email`, `course`) VALUES ('".$_POST['fname']."','".$_POST['mobileno']."','".$_POST['email']."','".$_POST['message']."')";
