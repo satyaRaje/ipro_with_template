48946524faa2c800faa00d93e39a6cb6fa0f70d5
data <joshipoonam207@gmail.com>
2019-03-22 01:09:10 -0500

    mutation done

--- application/views/base_web/stud_register.php
+++ application/views/base_web/stud_register.php
@@ -8,55 +8,119 @@
                      <h2>Mutation Student Registration Form</h2>
                      <form class="form-material m-t-40">
                                     <div class="form-group">
-                                        <label>Default Text <span class="help"></span></label>
-                                        <input type="text" class="form-control form-control-line" value="Some text value..."> </div>
-                                    <div class="form-group">
                                         <label for="example-email">Email <span class="help"></span></label>
                                         <input type="email" id="example-email2" name="example-email" class="form-control" placeholder="Email"> </div>
+
+                                        <div class="form-group">
+                                        <label for="example-email">Full Name<span class="help"></span></label>
+                                        <input type="email" id="example-email2" name="example-email" class="form-control" placeholder="Email"> </div>
+                                    
                                     <div class="form-group">
-                                        <label>Password</label>
-                                        <input type="password" class="form-control" value="password"> </div>
+                                        <label for="example-email">Mobile no.<span class="help"></span></label>
+                                        <input type="email" id="example-email2" name="example-email" class="form-control" placeholder="Email"> </div>
+                                   
+                                   <div class="form-group">
+                                    <label for="example-email">What'sApp No.<span class="help"></span></label>
+                                        <input type="email" id="example-email2" name="example-email" class="form-control" placeholder="Email"> </div>
+
+                                     <div class="form-group">
+                                        <label for="example-email">Date of Birth<span class="help"></span></label>
+                                     <input class="form-control" type="date" value="2011-08-19" id="example-date-input"></div>
+
                                     <div class="form-group">
-                                        <label>Placeholder</label>
+                                        <label>College ID </label>
+                                        <input type="email" class="form-control" value=""> </div>
+
+                                    <div class="form-group">
+                                        <label>Year of Admission</label>
                                         <input type="text" class="form-control" placeholder="placeholder"> </div>
-                                    <div class="form-group">
-                                        <label>Text area</label>
-                                        <textarea class="form-control" rows="5"></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Input Select</label>
+
+                                        <div class="form-group">
+                                        <label>Year of Study</label>
                                         <select class="form-control">
-                                            <option>1</option>
-                                            <option>2</option>
-                                            <option>3</option>
-                                            <option>4</option>
-                                            <option>5</option>
+                                            <option>First year</option>
+                                            <option>Second Year</option>
+                                            <option>Third Year</option>
+                                            <option>Final Year</option>
+                                            <option>Other</option>
                                         </select>
                                     </div>
+
                                     <div class="form-group">
-                                        <label>File upload</label>
-                                        <div class="fileinput fileinput-new input-group" data-provides="fileinput">
-                                            <div class="form-control" data-trigger="fileinput"> 
-                                                <i class="glyphicon glyphicon-file fileinput-exists"></i> 
-                                                <span class="fileinput-filename">
-                                                </span>
-                                            </div>
-                                             <span class="input-group-addon btn btn-default btn-file">
-                                             <span class="fileinput-new">Select file</span>
-                                              <span class="fileinput-exists">Change</span>
-                                            <input type="hidden">
-                                            <input type="file" name="..."> 
-                                        </span>
-                                         <a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a> 
-                                     </div>
+                                        <label>Engineering/Diploma</label>
+                                        <select class="form-control">
+                                            <option>Direct Engineering Degree</option>
+                                            <option>Diploma Degree</option>
+                                            <option>Direct Second Year Degree</option>
+                                            <option>Architecture</option>
+                                            <option>M.E./M.Tech</option>
+                                            <option>Other</option>
+                                        </select>
                                     </div>
+                                    
+                                     <div class="form-group">
+                                        <label>College</label>
+                                        <select class="form-control">
+                                            <option>Marathwada Institute of Technology,Aurangabad</option>
+                                            <option>Maharashtra Institute of Technology,Aurangabad</option>
+                                            <option>CSMSS College of Engineering</option>
+                                            <option>Jawaharlal Nehru College of Engineering</option>
+                                            <option>PES College of Engineering</option>
+                                            <option>Other</option>
+                                        </select>
+                                    </div>
+
                                     <div class="form-group">
-                                        <label>Helping text</label>
-                                        <input type="text" class="form-control form-control-line"> 
-                                        <span class="help-block text-muted">
-                                            <small>A block of help text that breaks onto a new line and may extend beyond one line.</small>
-                                        </span>
-                                         </div>
+                                        <label>Department</label>
+                                        <select class="form-control">
+                                            <option>Mechanical Engineering</option>
+                                            <option>Civil Engineering</option>
+                                            <option>Plastic & Polymer Engineering</option>
+                                            <option>Computer Science & Engineering</option>
+                                            <option>Electrical & Electronics Engineering</option>
+                                            <option>Other</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Interests</label>
+                                        <select class="form-control">
+                                            <option>Engineering Design</option>
+                                            <option>Product Design</option>
+                                            <option>Architecture</option>
+                                            <option>Art & Craft</option>
+                                            <option>Manifacturing Process</option>
+                                            <option>Automobile</option>
+                                            <option>Aerospace</option>
+                                            <option>Medical/Health care</option>
+                                            <option>Exploring new Technologies</option>
+                                            <option>Website Design and App Development</option>
+                                        </select>
+                                    </div>
+
+                                     <div class="form-group">
+                                        <label>Future Goals</label>
+                                        <select class="form-control">
+                                            <option>Job</option>
+                                            <option>Masters/M.S.</option>
+                                            <option>Government Jobs</option>
+                                            <option>Startup</option>
+                                           </select>
+                                    </div>
+
+                                   
+                                    <div class="form-group">
+                                        <label for="example-email">Comments<span class="help"></span></label>
+                                        <input type="email" id="example-email2" name="example-email" class="form-control" placeholder="Email"> </div>
+
+
+                                             <button type="submit" class="btn btn-success waves-effect waves-light m-r-10" name="send">Send Message</button>
+                                    <button type="submit" class="btn btn-inverse waves-effect waves-light">Cancel</button>
+                                
+
+                                   
+
+
                                          </form>
                                      </div>
                                  </div>
