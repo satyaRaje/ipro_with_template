cdf2b27e9e9dfd149908084b5cc11736d32d55e6
swaranjalik24@gmail.com <swaranjalik24@gmail.com>
2019-03-30 20:17:12 +0530

    changes

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/test/ipro_with_template/';
+$config['base_url'] = 'http://localhost/ipro_with_template/';
 
 /*
 |--------------------------------------------------------------------------
